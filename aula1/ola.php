b62126203fbbef852c030f84e434cbbee9214594
murilovarges <murilo.varges@gmail.com>
2023-02-14 11:00:51 -0300

    Meu primeiro programa PHP.

---
+++ aula1/ola.php
@@ -0,0 +1,3 @@
+<?php
+    echo "<h1>Ola mundo</h1>";
+?>
---
+++ aula1/ola2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Aqui é o código HTML -->
+    <h1>Exemplo Olá Mundo</h1>
+    <?php
+        // Aqui é o código PHP
+        echo "<h2>Olá IFSP - PROFESSOR MURILO</h2>";
+    ?>    
+</body>
+</html>
